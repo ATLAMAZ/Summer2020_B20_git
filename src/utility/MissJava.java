37d2cb0597666cd484940b8663e390b806dcda3d
Ulku Firat Atlamaz <38175135+ATLAMAZ@users.noreply.github.com>
2020-08-05 21:35:28 -0400

    Update MissJava.java

--- src/utility/MissJava.java
+++ src/utility/MissJava.java
@@ -2,6 +2,7 @@ package utility;
 
 public class MissJava {
     public static void main(String[] args) {
-        System.out.println("I miss java");
+        System.out.println("I miss java class");
+        // This line from Github
     }
 }
