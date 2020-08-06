cf5b1a55e42c2fe95f1ab86b0e6e8902d64131c7
Ulku Firat Atlamaz <38175135+ATLAMAZ@users.noreply.github.com>
2020-08-05 21:32:48 -0400

    Update GitFun.java

--- src/utility/GitFun.java
+++ src/utility/GitFun.java
@@ -3,6 +3,7 @@ package utility;
 public class GitFun {
 
     public static void main(String[] args) {
-        System.out.println("Everthing is fun");
+        System.out.println("Everthing is fun, isn't it");
+        // this line from github
     }
 }
